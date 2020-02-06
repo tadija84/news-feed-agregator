830bd38b578ca4204daccd2b8a206d84fa8719cb
Finn <10346401+finncodes@users.noreply.github.com>
2020-02-06 19:42:55 +1300

    Switched from Fastify to Express

--- app.js
+++ app.js
@@ -1,52 +1,26 @@
-const fastify = require('fastify')({ logger: true });
-const fs = require('fs');
+const express = require('express');
+const app = express();
+const path = require('path');
 const { existsAsync, setAsync, getAsync } = require('./redis');
 const sources = require('./sources');
 const moment = require('moment-timezone');
-const fnv1a = require('@sindresorhus/fnv1a');
 
-fastify.register(require('point-of-view'), {
-    engine: {
-        ejs: require('ejs')
-    }
-});
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
-fastify.get('/favicon.ico', async (req, res) => {
-    const stream = fs.createReadStream('./favicon.ico');
-    res.type('image/x-icon').send(stream);
-    return res;
-});
-
-fastify.get('/', async (req, res) => {
+app.get('/', async (req, res) => {
     let articles = null;
-    let articlesHash = null;
 
     if (await existsAsync('articles') === 1) {
-        articlesHash = await getAsync('hash');
-        if (req.headers['if-none-match'] === `"${articlesHash}"`) {
-            res.status(304);
-            res.res.end();
-
-            res.sent = true;
-            return res;
-        }
         articles = JSON.parse(await getAsync('articles'));
     } else {
         articles = await sources();
         articles.sort((a, b) => new Date(b.date) - new Date(a.date));
 
-        const articlesString = JSON.stringify(articles);
-        articlesHash = fnv1a(articlesString);
-        
-        await setAsync('articles', articlesString, 'EX', 1800);
-        await setAsync('hash', articlesHash, 'EX', 1800);
+        await setAsync('articles', JSON.stringify(articles), 'EX', 1800);
     };
 
-    res.header('ETag', `"${articlesHash}"`);
-    res.view('index.ejs', { articles, moment });
-    return res;
+    return res.render('index', { articles, moment });
 });
 
-fastify.listen(process.env.PORT || 3000, (err, addr) => {
-    console.log('Server running on ' + addr);
-});
+app.listen(process.env.PORT || 3000);
